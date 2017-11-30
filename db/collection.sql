a50f7b9949bf511bece46c416b2ac466635442eb
Jukka Hyytiälä <jukka.hyytiala@suoratyo.fi>
2017-11-30 23:30:59 +0200

    Add collection

---
+++ db/collection.sql
@@ -0,0 +1,27 @@
+-- depends_on: user_management.sql
+
+DO LANGUAGE plpgsql $wrapper$
+BEGIN
+
+  IF NOT _v.is_executed('collection.sql', 'initial-version') THEN
+    PERFORM _v.set_executed('collection.sql', 'initial-version');
+
+    CREATE TABLE comicstor.collection (
+      id SERIAL PRIMARY KEY,
+      name CHARACTER VARYING(20) NOT NULL,
+      owner_id INTEGER NOT NULL REFERENCES comicstor.user (id) ON DELETE CASCADE
+    );
+
+    GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE comicstor.collection TO comicstor_user;
+    ALTER TABLE comicstor.collection ENABLE ROW LEVEL SECURITY;
+
+  END IF;
+
+END;
+$wrapper$;
+
+DROP POLICY IF EXISTS all_collection ON comicstor.collection;
+CREATE POLICY all_collection
+ON comicstor.collection FOR ALL TO comicstor_user
+USING (owner_id = comicstor.current_user_id())
+WITH CHECK (owner_id = comicstor.current_user_id());
